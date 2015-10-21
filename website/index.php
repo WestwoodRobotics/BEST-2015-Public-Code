8f7dd32c6b0399917a4f9e81272cbe4b65a790b6
HexR Web <hello@hexr.org>
2015-10-21 09:57:19 -0500

    Added Gallery, bells
    Added more text, easter eggs and custom gallery

---
+++ website/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+	<head>
+		<title>BEST Website... Coming soon! | Westwood Robotics</title>
+		 <style>
+			body {background: white}
+			section
+			{
+				color:orange;
+				position: inherit;
+				margin-top:25%;
+				text-align:center;
+			}
+			h1
+			{
+				font-size:2.5em;
+			}
+			p
+			{
+				font-size:1.5em;
+			}
+	  </style>
+	</head>
+	<body>
+		<section class="valign">
+			<h1 class="center">Websites created by Westwood Robotics for BEST...</h1>
+			<p class="center"><strong>...coming soon!</strong></p>
+			<p>Back to <a href="http://team2583.org/best">BEST page</a> or <a href="http://team2583.org/">home</a></p>
+		</section>
+</html>
--- website/js/init.js
+++ website/js/init.js
@@ -20,14 +20,14 @@ $(document).ready(function()
 		{
 			$("#map .map-section").removeClass("active");
 			$("#map .sublevel-"+(b-1)).addClass("active");
-			
 			$("#section-"+b).find(".animateMe").removeClass("hide");
 			$("#section-"+b).find(".animateMe").removeClass("animated fadeOut card-animation").addClass("animated card-animation fadeIn");
+			$("#toast-container").delay(1500).addClass("animated pulse")
+			setTimeout(function(){$("#toast-container").removeClass("animated pulse");},1500)
 		},
 		afterSlideLoad: function(a,b,c,d)
 		{
 			$("#map .map-section").removeClass("active");
-			
 			$("#section-3").find(".animateMe").removeClass("hide");
 			$("#section-4").find(".animateMe").removeClass("hide");
 			if(c === 0)
@@ -35,11 +35,13 @@ $(document).ready(function()
 				$("#section-3").find(".animateMe").removeClass("animated fadeOut card-animation").addClass("animated card-animation fadeIn");
 				$("#map .sublevel-2").addClass("active");
 			}
-			else 
+			else
 			{
 				$("#section-4").find(".animateMe").removeClass("animaed fadeOut card-animation").addClass("animated card-animation fadeIn");
 				$("#map .sublevel-3").addClass("active");
 			}
+			$("#toast-container").delay(1500).addClass("animated pulse")
+			setTimeout(function(){$("#toast-container").removeClass("animated pulse");},1500)
 		}
 	});
 	$(".map-section.sublevel-0").click(function(){$.fn.fullpage.moveSlideLeft();$.fn.fullpage.moveTo(1,0);});
@@ -53,7 +55,7 @@ $(document).ready(function()
 		else if($("#map .active").attr("class").indexOf("2") > 0){$.fn.fullpage.moveSlideRight();}
 	});
 	window.sitemap = true;
-	Materialize.toast("<a class='modal-trigger modal-action modal-open' href='#sitemap' id='sitemap-trigger'>The Miner's Guide</a>");
+	Materialize.toast("<a class='modal-trigger modal-action modal-open' href='#sitemap' id='sitemap-trigger'>The Miner's Guide / SiteMap</a>");
 	$('#sitemap-trigger').leanModal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
       in_duration: 300, // Transition in duration
@@ -61,7 +63,6 @@ $(document).ready(function()
       ready: function() {$("#toast-container").hide();}, // Callback for Modal open
       complete: function() {$("#toast-container").show();} // Callback for Modal close
     });
-	
 	$('#best-game-video-trigger').leanModal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
       in_duration: 300, // Transition in duration
@@ -91,6 +92,33 @@ $(document).ready(function()
       complete: function() {} // Callback for Modal close
     });
 	$("#ground-ul-tabs").tabs();
+	$("#section-3-tabs").tabs();
 	$("#robot-progression-slider .slider").slider({height:350}).slider("pause");
-	$("#our-team-slider .slider").slider({height:350,interval:1500});
+
+	 function e() {
+        function f(e) {
+            if (e < 10) e = "0" + e;
+            return e;
+        }
+        var t = new Date();
+        var n = t.getHours();
+        var r = t.getMinutes();
+        var i = t.getSeconds();
+        var s = "am";
+        if (n > 12) {
+            s = "pm";
+            n = n - 12;
+        }
+        r = f(r);
+        i = f(i);
+        var o = "  :  ";
+        var u = n + o + r + o + i + " " + s;
+        $($(".currentTime")[0]).html(u);
+        var a = setTimeout(function() {
+            e();
+        }, 500);
+    }
+    e();
+    $("#toast-container").delay(1500).addClass("animated pulse").removeClass("animated pulse");
+    setTimeout(function(){$("#toast-container").delay(1500).addClass("animated pulse")}, 3000);
 });
